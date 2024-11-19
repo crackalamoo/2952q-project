fix cuda OOM with gradient

esm_2952q.py
@@ -1,6 +1,7 @@
 import os
 os.environ['HF_HOME'] = '~/scratch/huggingface'
 
+import sys
 import torch
 from transformers import AutoTokenizer, EsmForProteinFolding
 
@@ -93,8 +94,12 @@ def _my_rel_pos_forward(pairwise_pos_e, residue_index, mask=None):
 
 def _my_trunk_forward(trunk, seq_feats, pair_feats, true_aa, residx, mask, no_recycles):
     device = seq_feats.device
-    s_s_0 = seq_feats
-    s_z_0 = pair_feats
+    device2 = trunk.trunk2sm_s.weight.device
+    s_s_0 = seq_feats.to(device2)
+    s_z_0 = pair_feats.to(device2)
+    true_aa = true_aa.to(device2)
+    residx = residx.to(device2)
+    mask = mask.to(device2)
     if no_recycles is None:
         no_recycles = trunk.config.max_recycles
     else:
@@ -111,13 +116,13 @@ def _my_trunk_forward(trunk, seq_feats, pair_feats, true_aa, residx, mask, no_re
     s_z = s_z_0
     recycle_s = torch.zeros_like(s_s)
     recycle_z = torch.zeros_like(s_z)
-    recycle_bins = torch.zeros(*s_z.shape[:-1], device=device, dtype=torch.int32)
+    recycle_bins = torch.zeros(*s_z.shape[:-1], device=device2, dtype=torch.int32)
 
     for recycle_idx in range(no_recycles):
         with ContextManagers([] if recycle_idx == no_recycles - 1 else [torch.no_grad()]):
-            recycle_s = trunk.recycle_s_norm(recycle_s.detach()).to(device)
-            recycle_z = trunk.recycle_z_norm(recycle_z.detach()).to(device)
-            recycle_z += trunk.recycle_disto(recycle_bins.detach()).to(device)
+            recycle_s = trunk.recycle_s_norm(recycle_s.detach()).to(device2)
+            recycle_z = trunk.recycle_z_norm(recycle_z.detach()).to(device2)
+            recycle_z += trunk.recycle_disto(recycle_bins.detach()).to(device2)
 
             s_s, s_z = trunk_iter(s_s_0 + recycle_s, s_z_0 + recycle_z, residx, mask)
 
@@ -138,8 +143,11 @@ def _my_trunk_forward(trunk, seq_feats, pair_feats, true_aa, residx, mask, no_re
                 trunk.recycle_bins,
             )
 
-    structure["s_s"] = s_s
-    structure["s_z"] = s_z
+    structure["s_s"] = s_s.to(device)
+    structure["s_z"] = s_z.to(device)
+    structure["positions"] = structure["positions"].to(device)
+    # for key in structure.keys():
+    #     structure[key] = structure[key].to(device)
 
     return structure
 
@@ -206,7 +214,7 @@ def my_forward(tokenizer, model, sequences):
     print("trunk:", model.trunk.trunk2sm_s.weight.dtype)
     print(torch.cuda.memory_summary(device=model.device))
 
-    # structure = model.trunk(s_s_0, s_z_0, aa, position_ids, attention_mask, no_recycles=num_recycles)
+    sys.stdout.flush()
     structure = _my_trunk_forward(model.trunk, s_s_0, s_z_0, aa, position_ids, attention_mask, no_recycles=num_recycles)
     print("Got structure")
     structure = {

uat.py
@@ -3,13 +3,15 @@ import sys
 import torch
 import esm_2952q as esm
 import uniprot
+from torch.utils.checkpoint import checkpoint
 
 def get_trigger(tokenizer, model, seqs, device):
     trigger = 'G' * 25 # TODO: discover actual trigger
     
-    # with torch.cuda.amp.autocast():
-    with torch.no_grad():
-        outputs, embeddings = esm.my_forward(tokenizer, model, seqs)
+    with torch.cuda.amp.autocast():
+    # with torch.no_grad():
+        custom_forward = lambda seqs: esm.my_forward(tokenizer, model, seqs)
+        outputs, embeddings = checkpoint(custom_forward, seqs)
     print(outputs.positions)
 
     pdb = esm.convert_outputs_to_pdb(outputs)
@@ -26,15 +28,10 @@ if __name__ == '__main__':
     torch.backends.cuda.matmul.allow_tf32 = True
     model.trunk.set_chunk_size(1)
     model.trunk.config.max_recycles = 1
-    model.trunk = model.trunk.half()
-    model.trunk.pairwise_positional_embedding = model.trunk.pairwise_positional_embedding.half()
-    model.distogram_head = model.distogram_head.half()
-    model.lddt_head = model.lddt_head.half()
-    model.ptm_head = model.ptm_head.half()
-    model.lm_head = model.lm_head.half()
+    # model.trunk.to('cpu')
 
     # seqs = esm.test_proteins
-    seqs = uniprot.read_seqs_list()[:1]
+    seqs = uniprot.read_seqs_list()[:1][:4]
     print(seqs)
 
     torch.cuda.empty_cache()