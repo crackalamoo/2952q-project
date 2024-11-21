e08ac654879e17ce96c53b2633fefcca57d2a7c8
crackalamoo <harysdalvi@gmail.com>
2024-11-21 16:56:53 -0500

    compare real protein, glycine trigger, and discovered trigger

--- uat.py
+++ uat.py
@@ -12,10 +12,38 @@ def get_trigger(tokenizer, model, seqs, steps, device):
 
     aa_emb = esm.tokenize_and_embed(tokenizer, model, esm.aa).to('cpu')
     print(aa_emb.shape)
+    chunk_size = 9
+    with torch.no_grad():
+
+        print("Sequence from database:", seqs[0])
+        outputs, _ = esm.my_forward(tokenizer, model, [seqs[0]], '')
+        pdb = esm.convert_outputs_to_pdb(outputs)
+        esm.save_pdb(pdb, 'output_structure.pdb')
+
+        avg_loss = 0
+        for i, seq in enumerate(seqs):
+            outputs, trigger_embeds = esm.my_forward(tokenizer, model, [seq], trigger)
+            error = outputs.predicted_aligned_error.to(dtype=torch.float16)
+            loss = 0
+            for j in range(0, error.size(1), chunk_size):
+                sub_error = error[0, j:j+chunk_size, j:j+chunk_size]
+                sub_error = torch.diag(sub_error)
+                sub_loss = -torch.mean(sub_error) # high error = low loss (for trigger)
+                loss += sub_loss.item()
+                del sub_error
+                del sub_loss
+            avg_loss += loss
+            if i == 0:
+                print("Initial sequence:", seq, trigger)
+                pdb = esm.convert_outputs_to_pdb(outputs)
+                esm.save_pdb(pdb, 'initial_structure.pdb')
+        avg_loss /= len(seqs)
+        print("Initial loss:", avg_loss)
 
     for step in range(steps):
         grad = None
         print(f"STEP {step+1}/{steps}")
+        avg_loss = 0
         for seq in seqs:
             # with torch.no_grad():
             print("sequence len:", len(seq))
@@ -28,7 +56,6 @@ def get_trigger(tokenizer, model, seqs, steps, device):
             error = outputs.predicted_aligned_error.to(torch.float16)
             # print(error.shape)
             # print(trigger_embeds.shape)
-            chunk_size = 9
             loss = 0
             emb_grad = torch.zeros_like(trigger_embeds)
             assert error.size(0) == 1
@@ -52,13 +79,15 @@ def get_trigger(tokenizer, model, seqs, steps, device):
                 del sub_error
             print("Loss:", loss)
             print(f"Memory after full backward: {torch.cuda.memory_allocated() / 1e6:.2f} MB")
-            # print("trigger_embeddings grad:", emb_grad)
-            # print("trigger_embeddings grad:", emb_grad.shape)
             emb_grad = emb_grad.detach().to('cpu')[0, :, :] # trigger_len x dim
             grad = emb_grad if grad is None else grad + emb_grad
             del outputs
             del error
             del trigger_embeds
+            avg_loss += loss
+        avg_loss /= len(seqs)
+        print("AVERAGE LOSS:", avg_loss)
+
         with torch.no_grad():
             # compute new trigger
             trigger_embeds = esm.tokenize_and_embed(tokenizer, model, trigger).to('cpu') # trigger_len x dim
@@ -68,35 +97,34 @@ def get_trigger(tokenizer, model, seqs, steps, device):
             for i in range(trigger_embeds.size(1)):
                 trigger_i = trigger_embeds[:, i, :]
                 trigger_i = torch.repeat_interleave(trigger_i, 20, dim=0)
-                # print("trigger_i:", trigger_i.shape, trigger_i.device)
                 diff = aa_emb.squeeze() - trigger_i
-                # print("diff:", diff.shape, diff.device)
                 grad_i = emb_grad[i, :]
-                # print("grad_i:", grad_i.shape, grad_i.device)
                 dot_prod = diff * grad_i
                 dot_prod = torch.sum(dot_prod, dim=1)
-                # print("dot_prod:", dot_prod.shape)
                 min_aa = torch.argmin(dot_prod).item()
-                # print("min_aa:", min_aa)
                 trigger = trigger[:i] + esm.aa[min_aa] + trigger[i+1:]
             print("Updated trigger:", trigger)
 
     with torch.no_grad():
-        print(f"Memory before no_grad forward: {torch.cuda.memory_allocated() / 1e6} MB") 
-        outputs, trigger_embeds = esm.my_forward(tokenizer, model, [seqs[0]], trigger)
-        print(f"Memory after no_grad forward: {torch.cuda.memory_allocated() / 1e6} MB")
-        error = outputs.predicted_aligned_error.to(dtype=torch.float16)
-        loss = 0
-        for j in range(0, error.size(1), chunk_size):
-            sub_error = error[0, j:j+chunk_size, j:j+chunk_size]
-            sub_error = torch.diag(sub_error)
-            sub_loss = -torch.mean(sub_error) # high error = low loss (for trigger)
-            loss += sub_loss.item()
-            del sub_error
-            del sub_loss
-        print("Final loss:", loss)
-    pdb = esm.convert_outputs_to_pdb(outputs)
-    esm.save_pdb(pdb, 'output_structure.pdb')
+        avg_loss = 0
+        for i, seq in enumerate(seqs):
+            outputs, trigger_embeds = esm.my_forward(tokenizer, model, [seq], trigger)
+            error = outputs.predicted_aligned_error.to(dtype=torch.float16)
+            loss = 0
+            for j in range(0, error.size(1), chunk_size):
+                sub_error = error[0, j:j+chunk_size, j:j+chunk_size]
+                sub_error = torch.diag(sub_error)
+                sub_loss = -torch.mean(sub_error) # high error = low loss (for trigger)
+                loss += sub_loss.item()
+                del sub_error
+                del sub_loss
+            avg_loss += loss
+            if i == 0:
+                print("Final sequence:", seq, trigger)
+                pdb = esm.convert_outputs_to_pdb(outputs)
+                esm.save_pdb(pdb, 'final_structure.pdb')
+        avg_loss /= len(seqs)
+        print("Final loss:", avg_loss)
 
     return trigger
 
@@ -122,6 +150,6 @@ if __name__ == '__main__':
     print([(seq, len(seq)) for seq in seqs])
 
     torch.cuda.empty_cache()
-    trigger = get_trigger(tokenizer, model, seqs, 8, device)
+    trigger = get_trigger(tokenizer, model, seqs, 2, device)
     print("Trigger:", trigger)
     print(f"Done: {time.time() - start_time} s")
