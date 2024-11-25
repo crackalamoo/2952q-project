e7e8803299b3c2f1ff4ebfc59ab1f05562aabdfa
crackalamoo <harysdalvi@gmail.com>
2024-11-25 00:31:19 -0500

    query PDB structure data from RCSB

--- uniprot.py
+++ uniprot.py
@@ -1,6 +1,7 @@
 import requests
 from io import BytesIO
 import pandas as pd
+from esm_2952q import save_pdb
 
 def get_monomers(taxonomy_id, min_len, max_len):
     uniprot_url = f"https://rest.uniprot.org/uniprotkb/stream?compressed=true&fields=accession%2Csequence&format=tsv&query=%28%28taxonomy_id%3A{taxonomy_id}%29%20AND%20%28reviewed%3Atrue%29%20AND%20%28length%3A%5B{min_len}%20TO%20{max_len}%5D%29%20AND%20%28cc_subunit%3Amonomer%29%29"
@@ -23,6 +24,16 @@ def read_seqs_list():
     df = pd.read_csv('~/scratch/bio-out/seqs-df.csv', compression='gzip', sep='\t')
     return df['Sequence'].tolist()
 
+def rcsb_sequence(uniprot_id):
+    uniprot_data = requests.get(f"https://rest.uniprot.org/uniprotkb/search?query={uniprot_id}&fields=structure_3d").json()
+    try:
+        rcsb_id = uniprot_data['results'][0]['uniProtKBCrossReferences'][0]['id']
+        rcsb_data = requests.get(f'https://files.rcsb.org/download/{rcsb_id}.pdb')
+        return rcsb_data.text
+    except IndexError:
+        print(uniprot_data['results'])
+        return None
+
 
 if __name__ == '__main__':
     df = get_ecoli_seqs(min_len=5,max_len=100)
@@ -30,4 +41,11 @@ if __name__ == '__main__':
     seqs = read_seqs_list()
     print(seqs)
     print(len(seqs))
+
+    for entry in df['Entry']:
+        print(entry)
+        pdb = rcsb_sequence(entry)
+        if pdb is not None:
+            print("saving:", entry)
+            save_pdb([pdb], f"rcsb/{entry}.pdb")
 
